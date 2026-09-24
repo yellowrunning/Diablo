2b2370dbf9ce1964c9f151c06897e06db172c0de
Dennis Huang <dennis.huang@learnet.se>
2026-09-24 14:54:12 +0200

    Add project files.

---
+++ Diablo/Diablo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+void Functions::Gameloop(Player& aPlayer);
+
+// 1. Attack
+// 2. Inventory
+// 3. Stats
+// 4. Run
+
+
+void MainMenu()
+{
+	std::cout << "=====DIABLO=====\n";
+}
